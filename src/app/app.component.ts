5bec714c6549a87dea10d12fc7ca868b9c64d74d
aishvarya779 <aishvaryatiwari123@gmail.com>
2024-12-21 20:04:19 +0530

    coloring tool bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
+
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { NavListComponent } from './nav-list/nav-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
     NavListComponent,
-    MatButtonModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatMenuModule,
     MatSidenavModule,
+    HeaderComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
