648b161bc72e75a5a3c9c478566776d321614fe2
aishvarya779 <aishvaryatiwari123@gmail.com>
2024-12-21 20:37:53 +0530

    songs list added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-home',
-  imports: [MatSlideToggleModule],
+  imports: [MatInputModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  playList: any[] = [];
+  ngOnInit(): void {
+    this.listSongs();
+  }
+  async listSongs(event?: any) {
+    console.log(event);
+    const url = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${event?.target.value || 'eminem'}`;
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'cXI3nwKbIwmshrsTAl3q6f53EPiSp1XZRotjsnkqm7643CAiiD',
+		'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com'
+	}
+};
 
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+  this.playList = result.data;
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+  }
 }
